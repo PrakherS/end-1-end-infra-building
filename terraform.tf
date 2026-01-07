ca31cd6bdc4a397c79157bc46b189df8365adf90
Prakher Singhal <prakhersinghal@gmail.com>
2026-01-07 20:36:30 +0530

    Provider setup done. Connection to AWS cloud tested

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=1.8.4"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  shared_credentials_files = ["C:\\Users\\prakh\\.aws\\credentials"]
+  #   access_key = "my-access-key"
+  #   secret_key = "my-secret-key"
+}
